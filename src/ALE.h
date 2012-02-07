ed96a01fbbcc5a9fe062e2cd92b88e9ce21bbce1
Scott Clark <sc932@cornell.edu>
2012-02-06 21:30:53 -0500

    moved opts/args strings to ALE.h

--- src/ALE.h
+++ src/ALE.h
@@ -11,4 +11,9 @@
 #include "ALElike.h"
 #include "geneTree.h"
 
+static const char WELCOME_MSG[80] = "Welcome to the Assembly Likelihood Estimator!\n(C) 2010 Scott Clark\n\n";
+static const char USAGE[80] = "Usage: %s [-options] readSorted.[s|b]am assembly.fasta[.gz] ALEoutput.txt\n";
+static const char SHORT_OPTIONS[80] = "    Options:\n    -h : print out help\n";
+static const char LONG_OPTIONS[1024] = "Options: <i>nt <f>loat [default]\n  -h        : print out this help\n\n   -kmer <f> : Kmer depth for kmer stats [4]\n  -qOff <i> : Quality ascii offset (illumina) [33] or 64 (or 0)\n  -pl placementOutputBAM\n\n\n";
+
 #endif
