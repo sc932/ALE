5024fd8533a1dd2462ee7bbb76050097b61e7a94
Rob Egan <regan@dakar.jgi-psf.org>
2012-02-16 11:26:02 -0800

    fixed for new ALE output

--- src/ale2wiggle.py
+++ src/ale2wiggle.py
@@ -3,8 +3,8 @@
 def convertToWiggle(inFile):
 	kwig = open(inFile + "-kmer.wig", 'w')
 	pwig = open(inFile + "-place.wig", 'w')
+	iwig = open(inFile + "-insert.wig", 'w')
 	dwig = open(inFile + "-depth.wig", 'w')
-	twig = open(inFile + "-total.wig", 'w')
 	wig = open(inFile + ".wig", 'w')
 
 	for line in file(inFile):
@@ -12,17 +12,17 @@ def convertToWiggle(inFile):
 		if line[0] == "#":
 			sp = line.split()
 			if sp[1] == "Reference:":
-				pwig.write("track name=ALE-p color=0,0,255 group=ALE priority=1\nfixedStep chrom=" + sp[2] + " start=0 step=1\n")
-				dwig.write("track name=ALE-d color=255,0,0 group=ALE priority=2\nfixedStep chrom=" + sp[2] + " start=0 step=1\n")
-				kwig.write("track name=ALE-k color=0,255,0 group=ALE priority=3\nfixedStep chrom=" + sp[2] + " start=0 step=1\n")
-				twig.write("track name=ALE-t color=0,0,0 group=ALE priority=4\nfixedStep chrom=" + sp[2] + " start=0 step=1\n")
+				pwig.write("track name=ALE-place color=0,0,255 group=ALE priority=1\nfixedStep chrom=" + sp[2] + " start=0 step=1\n")
+				iwig.write("track name=ALE-insert color=0,0,255 group=ALE priority=1\nfixedStep chrom=" + sp[2] + " start=0 step=1\n")
+				dwig.write("track name=ALE-depth color=255,0,0 group=ALE priority=2\nfixedStep chrom=" + sp[2] + " start=0 step=1\n")
+				kwig.write("track name=ALE-kmer color=0,255,0 group=ALE priority=3\nfixedStep chrom=" + sp[2] + " start=0 step=1\n")
 				wig.write("track name=depth color=0,0,0\nfixedStep chrom=" + sp[2] + " start=0 step=1\n")
 			continue
-		contig,position,depth,depthLike,placeLike,kmerLike,totalLike = line.split()
+		contig,position,depth,depthLike,placeLike,insertLike,kmerLike = line.split()
 		kwig.write(str(kmerLike) + "\n")	
-		pwig.write(str(placeLike) + "\n")	
+		pwig.write(str(placeLike) + "\n")
+		iwig.write(str(insertLike) + "\n")	
 		dwig.write(str(depthLike) + "\n")	
-		twig.write(str(totalLike) + "\n")	
 		wig.write(str(depth) + "\n")
 
 def main(argv):
